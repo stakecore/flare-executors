fb7e539c3b9b7661d1a048b7f150ec5969959412
kuco23 <nseverkar@gmail.com>
2025-08-03 22:49:05 +0200

    feat: add airdrop executor

---
+++ airdrop-reward-executor.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+source <(grep -v '^#' "./.env.flr" | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
+
+month=$1
+
+cast send $DISTRIBUTION_TO_DELEGATORS "claim(address,address,uint256,bool)" \
+    $AIRDROP_REWARD_OWNER $AIRDROP_REWARD_RECIPIENT $month true \
+    --rpc-url $RPC_URL --private-key $AIDROP_EXECUTOR_PVK
